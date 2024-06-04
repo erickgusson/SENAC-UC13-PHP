236bdb3c6b245d0b78dd5361ccb8da5f14786c94
Erick Gusson <80285372+erickgusson@users.noreply.github.com>
2024-06-04 10:57:55 -0300

    Fazendo o sistema de implementação de produtos

---
+++ Aula_02/pages/produto.php
@@ -0,0 +1,42 @@
+<section id="produtos">
+    <h2 class="titulo">Produtos</h2>
+    <main class="container">
+        <div class="row">
+
+        <?php foreach ($produtos as $key => $info) { ?>
+
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <figure>
+                    <img src="./img/<?= $info['poster'] ?>" alt="Poster do jogo" class="foto-produto">
+                    <figcaption>
+                        <h4><?= $info['titulo'] ?></h4>
+                        <span class="preco">R$ <?= $info['preco'] ?>,99</span>
+                        <span class="avaliacao">
+
+                            <?php for ($i = 0; $i < $info['avaliacao']; $i++) { ?>
+
+                                <i class="bi bi-star-fill"></i>
+
+                            <?php } ?>
+
+                            <?php for ($i = 0; $i < 5 - $info['avaliacao']; $i++) { ?>
+
+                                <i class="bi bi-star"></i>
+
+                            <?php } ?>
+                            <!-- <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-half"></i>
+                        <i class="bi bi-star"></i>
+                         -->
+                        </span>
+                    </figcaption>
+                </figure>
+            </div>
+
+        <?php } ?>
+
+        </div>
+    </main>
+</section>
