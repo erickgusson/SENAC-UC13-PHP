b63be6aee105202ddd7edb937db6dba3fc533181
Erick Gusson <80285372+erickgusson@users.noreply.github.com>
2024-06-11 09:08:24 -0300

    Atualização do nome das pastas

---
+++ Aula_04(pdo)/README.md
@@ -0,0 +1,1 @@
+# Acessando o PDO
---
+++ Aula_04(pdo)/index.php
@@ -0,0 +1,67 @@
+<?php
+
+include_once './pages/header.html';
+
+$produtos = [
+    [
+        'poster' => 'baldurs-gate-3.jpg',
+        'titulo' => 'Baldurs\'s Gate 3',
+        'preco' => 290.00,
+        'avaliacao' => 3
+    ],
+    [
+        'poster' => 'death-stranding.png',
+        'titulo' => 'Death Stranding',
+        'preco' => 139.00,
+        'avaliacao' => 2
+    ],
+    [
+        'poster' => 'jedi-survivor.png',
+        'titulo' => 'Jedi Survivor',
+        'preco' => 139.00,
+        'avaliacao' => 5
+    ],
+    [
+        'poster' => 'devil-may-cry5.png',
+        'titulo' => 'Devil May Cry 5',
+        'preco' => 1,
+        'avaliacao' => 4
+    ],
+    [
+        'poster' => 'ghost-of-tshushima.jpg',
+        'titulo' => 'Ghost Of Tshushima',
+        'preco' => 1,
+        'avaliacao' => 4
+    ],
+    [
+        'poster' => 'mirage.png',
+        'titulo' => 'Mirage',
+        'preco' => 1,
+        'avaliacao' => 3
+    ],
+    [
+        'poster' => 'requiem.png',
+        'titulo' => 'Requiem',
+        'preco' => 1,
+        'avaliacao' => 4
+    ],
+    [
+        'poster' => 'valhalla.jpg',
+        'titulo' => 'Valhalla',
+        'preco' => 1,
+        'avaliacao' => 3
+    ],
+    [
+        'poster' => 'wukong.png',
+        'titulo' => 'Wukong',
+        'preco' => 1,
+        'avaliacao' => 4
+    ]
+];
+
+/** PDO == PHP Document Object
+ * 
+ */
+
+
+include './pages/produto.php';
