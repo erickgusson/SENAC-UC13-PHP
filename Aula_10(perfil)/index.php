53737010a7bc3bb577ee6d816e80def18ad6837a
Erick Gusson <80285372+erickgusson@users.noreply.github.com>
2024-07-15 10:23:52 -0300

    Fazendo logins usnado o SESSION do PHP

---
+++ Aula_10(perfil)/index.php
@@ -0,0 +1,38 @@
+<?php include_once "menu.php"; ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title>Perfil</title>
+</head>
+<body >
+<main class="fundo">
+
+<div class="container">
+  <div class="row pt-5" >
+  
+    <div class="col-12 col-sm-8 col-md-6 col-lg-3 mt-5">
+      <div class="card" style="height:100%" >
+        <img class="card-img-top" src="./img/fundo2.jpg" alt="Bologna">
+        <div class="card-body text-center">
+          <img class="avatar rounded-circle" src="./img/1720811725.jpg">
+          <h4 class="card-title">Lanterna Verde</h4>
+          <h6 class="card-subtitle mb-2 text-muted">Policial Galatico</h6>
+          <p class="card-text"> A principal fonte de poder do Lanterna Verde é um Anel do Poder. 
+            Também conhecido como a arma mais poderosa do universo DC, ele funciona à base de força
+             de vontade e imaginação. Quando ativado, o anel é capaz de gerar um campo de força que 
+             oferece várias habilidades a seu portador. Dessa maneira, o Lanterna é capaz de voar, 
+             ficar sob a água</p>
+          <p>
+          
+  </div>
+</div>
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<!-- fim conteudo -->
+</body>
+</html>
